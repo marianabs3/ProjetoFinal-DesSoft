77de6434687e9a99328165f2913ede3ff0d12b3e
ivanabb2 <62958684+ivanabb2@users.noreply.github.com>
2020-06-12 17:59:49 -0300

    menu certin

--- menu.py
+++ menu.py
@@ -1,4 +1,18 @@
 import pygame
+import os
+from pygame.locals import *
+
+menu_img = pygame.image.load('imagens/tela_1.jpeg')
+menu_img1 = pygame.image.load('imagens/tela_2.jpeg')
+menu_img2 = pygame.image.load('imagens/tela_3.jpeg')
+
+INIT = 3
+INIT2 = 4
+INIT3 = 5
+GAME = 6
+QUIT = 7
+
+
 def MenuInicial(window):
 
     casa = True
@@ -6,13 +20,100 @@ def MenuInicial(window):
     # ===== Loop principal =====
     while casa:
         # ----- Trata eventos
+        clock = pygame.time.Clock()
+        window.blit(menu_img, (0,0))
+
         for event in pygame.event.get():
             # ----- Verifica consequências
             if event.type == pygame.QUIT:
                 pygame.quit()
+            
+            if event.type == pygame.KEYDOWN:
+            
+            # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_RETURN:
+                    casa = False
+                    return INIT2
+
+            # Verifica se soltou alguma tecla.
+            if event.type == pygame.KEYUP:
+                
+                if event.key == pygame.K_RETURN:
+                    casa = False
+                    return INIT2
 
         # ----- Gera saídas
-        window.fill((0, 0, 255))  # Preenche com a cor branca
+        #window.fill((0, 0, 255))  # Preenche com a cor branca
 
         # ----- Atualiza estado do jogo
         pygame.display.update()  # Mostra o novo frame para o jogador
+
+def MenuInicial2(window):
+
+    bola = True
+
+    # ===== Loop principal =====
+    while bola:
+        # ----- Trata eventos
+        clock = pygame.time.Clock()
+        window.blit(menu_img1, (0,0))
+
+        for event in pygame.event.get():
+            # ----- Verifica consequências
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            
+            if event.type == pygame.KEYDOWN:
+            
+            # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_SPACE:
+                    bola = False
+                    return INIT3
+
+            # Verifica se soltou alguma tecla.
+            if event.type == pygame.KEYUP:
+                
+                if event.key == pygame.K_SPACE:
+                    bola = False
+                    return INIT3
+        # ----- Gera saídas
+        #window.fill((0, 0, 255))  # Preenche com a cor branca
+
+        # ----- Atualiza estado do jogo
+        pygame.display.update()  # Mostra o novo frame para o jogador
+
+def MenuInicial3(window):
+
+    amor = True
+    # ===== Loop principal =====
+    while amor:
+        # ----- Trata eventos
+        clock = pygame.time.Clock()
+        window.blit(menu_img2, (0,0))
+
+        for event in pygame.event.get():
+            # ----- Verifica consequências
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            
+            if event.type == pygame.KEYDOWN:
+            
+            # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_RIGHT:
+                    amor = False
+                    return GAME
+            # Verifica se soltou alguma tecla.
+            if event.type == pygame.KEYUP:
+                
+                if event.key == pygame.K_RIGHT:
+                    amor = False
+                    return GAME
+        
+
+        # ----- Gera saídas
+        #window.fill((0, 0, 255))  # Preenche com a cor branca
+
+        # ----- Atualiza estado do jogo
+        pygame.display.update()  # Mostra o novo frame para o jogador
+
+	
