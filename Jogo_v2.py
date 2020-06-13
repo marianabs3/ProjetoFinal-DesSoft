6e48a16386a574c11f05154755d38ccafba03684
ivanabb2 <62958684+ivanabb2@users.noreply.github.com>
2020-06-13 16:34:35 -0300

    tela final errada

--- Jogo_v2.py
+++ Jogo_v2.py
@@ -8,6 +8,7 @@ import random
 from menu import MenuInicial
 from menu import MenuInicial2
 from menu import MenuInicial3
+from menu import end_screen
 from Jogo_v1 import tela1
 
 
@@ -24,7 +25,8 @@ INIT = 3
 INIT2 = 4
 INIT3 = 5
 GAME = 6
-QUIT = 7
+END = 7
+QUIT = 8
 
 TILE_SIZE = 60
 
@@ -491,9 +493,9 @@ def game_screen(tela):
             all_sprites.add(r)
             all_guardas.add(r)
 
-        if lives == 0:    
-            pygame.quit()
-            
+        if lives == 0:
+            end_screen(tela)   
+    
         for i in range(lives):
             lives_rect.bottomleft = (10 + i*(LIVES_WIDTH-20), HEIGHT - 10)
             tela.blit(lives_img, lives_rect)
@@ -515,5 +517,7 @@ while state != QUIT:
         state = MenuInicial3(tela)
     elif state == GAME:
         state = game_screen(tela)
+    elif state == END:
+        state = end_screen(tela)
     else:
         state = QUIT
--- menu.py
+++ menu.py
@@ -10,7 +10,8 @@ INIT = 3
 INIT2 = 4
 INIT3 = 5
 GAME = 6
-QUIT = 7
+END = 7
+QUIT = 8
 
 
 def MenuInicial(window):
@@ -108,6 +109,40 @@ def MenuInicial3(window):
                 if event.key == pygame.K_RIGHT:
                     amor = False
                     return GAME
+        
+
+        # ----- Gera saídas
+        #window.fill((0, 0, 255))  # Preenche com a cor branca
+
+        # ----- Atualiza estado do jogo
+        pygame.display.update()  # Mostra o novo frame para o jogador
+
+def end_screen(window):
+
+    dado = True
+    # ===== Loop principal =====
+    while dado:
+        # ----- Trata eventos
+        clock = pygame.time.Clock()
+        window.blit(menu_img2, (0,0))
+
+        for event in pygame.event.get():
+            # ----- Verifica consequências
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            
+            if event.type == pygame.KEYDOWN:
+            
+            # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_p:
+                    dado = False
+                    return QUIT
+            # Verifica se soltou alguma tecla.
+            if event.type == pygame.KEYUP:
+                
+                if event.key == pygame.K_p:
+                    dado = False
+                    return QUIT
         
 
         # ----- Gera saídas
